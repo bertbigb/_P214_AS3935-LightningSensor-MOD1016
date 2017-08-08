ca9699a1068052e5bf80efea1666a24433ad03a8
bertbigb <bert@buiskool.net>
2017-08-08 07:06:13 +0200

    first append
    start building the sketch, learning

--- AS3935-LightningSensor-MOD1016.ino
+++ AS3935-LightningSensor-MOD1016.ino
@@ -1,66 +1,128 @@
-+//#######################################################################################################
- +//#################### Plugin MOD-1016 Lightning Sensor ######################################
- +//#######################################################################################################
- +//Copyright (c) 2017, Embedded Adventures - All rights reserved.
- +//Contact us at source [at] embeddedadventures.com - www.embeddedadventures.com
- +
- +// IN PROGRESS EVERYTHING BELOW IS NOT FINISHED and are COPIED BITS AND PIECES
- + 
-  +#include <Wire.h>
-  +#include <AS3935.h>
-  +#include <SPI.h>
- +
- +#define PLUGIN_214
- +#define PLUGIN_ID_214         214
- +#define PLUGIN_NAME_214       "LightningSensor MOD1016"
- +#define PLUGIN_VALUENAME1_214 "Lightning"
- +#define PLUGIN_VALUENAME2_214 "Distance"
- +
- +#define IRQ_PIN 2
- +#define CS_PIN 10
+//#######################################################################################################
+//#################### Plugin MOD-1016 Lightning Sensor ######################################
+//#######################################################################################################
+//include embedded adventures libary https://github.com/embeddedadventures/MOD-1016
 
- +volatile bool detected = false;
 
- +
- +boolean Plugin_214(byte function, struct EventStruct *event, String& string)
- +{
- +  boolean success = false;
- +  byte timeout;
- +
- +  switch (function)
- +  {
- +    case PLUGIN_DEVICE_ADD:
- +      {
- +        Device[++deviceCount].Number = PLUGIN_ID_214;
- +        Device[deviceCount].Type = DEVICE_TYPE_I2C;
- +        Device[deviceCount].VType = SENSOR_TYPE_DUAL;
- +        Device[deviceCount].Ports = 0;
- +        Device[deviceCount].PullUpOption = false;
- +        Device[deviceCount].InverseLogicOption = false;
- +        Device[deviceCount].FormulaOption = true;
- +        Device[deviceCount].ValueCount = 2;
- +        Device[deviceCount].SendDataOption = true;
- +        Device[deviceCount].TimerOption = true;
- +        Device[deviceCount].GlobalSyncOption = true;
- +        break;
- +      }
- +
- +    case PLUGIN_GET_DEVICENAME:
- +      {
- +        string = F(PLUGIN_NAME_214);
- +        break;
- +      }
- +
- +    case PLUGIN_GET_DEVICEVALUENAMES:
- +      {
- +        strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_214));
- +        strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_214));
- +        break;
- +      }
- +
- +
- +    case PLUGIN_INIT:
- +      {
+// IN PROGRESS EVERYTHING BELOW IS NOT FINISHED and are COPIED BITS AND PIECES
+
++  #include <Wire.h>
++  #include <SPI.h>
+  #include <SoftwareSerial.h>
+  #include <AS3935.h>
+  /*
+  Copyright (c) 2017, Embedded Adventures
+  All rights reserved.
+  Contact us at source [at] embeddedadventures.com
+  www.embeddedadventures.com
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  - Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+  - Redistributions in binary form must reproduce the above copyright
+    notice, this list of conditions and the following disclaimer in the
+    documentation and/or other materials provided with the distribution.
+  - Neither the name of Embedded Adventures nor the names of its contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+  THE POSSIBILITY OF SUCH DAMAGE.
+  */
+
+  // AS3935 MOD-1016 Lightning Sensor ESPEasy test sketch
+  // Written originally by Embedded Adventures
+
+
+
+  #define PLUGIN_214
+  #define PLUGIN_ID_214         214
+  #define PLUGIN_NAME_214       "LightningSensor MOD1016"
+  #define PLUGIN_VALUENAME1_214 "Lightning"
+  #define PLUGIN_VALUENAME2_214 "Distance"
+
+  #define IRQ_PIN 2
+  #define CS_PIN 10
+  #define MOD1016_ADDRESS          (0x03)  // default address (0x03 = datasheet)
+
+  volatile bool detected = false;
+
+
+  boolean Plugin_214(byte function, struct EventStruct *event, String& string)
+  {
+    boolean success = false;
+    byte timeout;
+
+    switch (function)
+    {
+      case PLUGIN_DEVICE_ADD:
+        {
+          Device[++deviceCount].Number = PLUGIN_ID_214;
+          Device[deviceCount].Type = DEVICE_TYPE_I2C;
+          Device[deviceCount].VType = SENSOR_TYPE_DUAL;
+          Device[deviceCount].Ports = 0;
+          Device[deviceCount].PullUpOption = false;
+          Device[deviceCount].InverseLogicOption = false;
+          Device[deviceCount].FormulaOption = true;
+          Device[deviceCount].ValueCount = 2;
+          Device[deviceCount].SendDataOption = true;
+          Device[deviceCount].TimerOption = true;
+          Device[deviceCount].GlobalSyncOption = true;
+          break;
+        }
+
+        case PLUGIN_GET_DEVICENAME:
+        {
+          string = F(PLUGIN_NAME_214);
+          break;
+        }
+
+        case PLUGIN_GET_DEVICEVALUENAMES:
+        {
+          strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[0], PSTR(PLUGIN_VALUENAME1_214));
+          strcpy_P(ExtraTaskSettings.TaskDeviceValueNames[1], PSTR(PLUGIN_VALUENAME2_214));
+          break;
+        }
+
+
+        case PLUGIN_INIT:
+        {
+          addLog(LOG_LEVEL_INFO, (char*)"INIT : MOD-1016 (AS3935) Lightning Sensor");
+          //I2C
+          Wire.begin();
+          mod1016.init(IRQ_PIN);
+          //SPI
+          //SPI.begin();
+          //mod1016.init(IRQ_PIN, CS_PIN);
+
+          //Tune Caps, Set AFE, Set Noise Floor
+          autoTuneCaps(IRQ_PIN);
+
+          //mod1016.setTuneCaps(7);
+          mod1016.setOutdoors();
+          mod1016.setNoiseFloor(5);
+          String log = F("TUNE\tIN/OUT\tNOISEFLOOR");
+          log += "\n";
+          Hex TUNE = mod1016.getTuneCaps();
+          Bin IN_OUT = mod1016.getAFE();
+          Hex NOISEFLOOR = mod1016.getNoiseFloor();
+          log += TUNE "\t" IN_OUT "\t" NOISEFLOOR "\n";
+
+          pinMode(IRQ_PIN, INPUT);
+          attachInterrupt(digitalPinToInterrupt(IRQ_PIN), alert, RISING);
+          mod1016.getIRQ();
+          log += "after interrupt";
+
+
  +        addLog(LOG_LEVEL_INFO, (char*)"INIT : SRF01");
  +        Plugin_100_SRF_Pin = Settings.TaskDevicePin1[event->TaskIndex];
  +        Plugin_100_SRF = new SoftwareSerial(Plugin_100_SRF_Pin, Plugin_100_SRF_Pin);
